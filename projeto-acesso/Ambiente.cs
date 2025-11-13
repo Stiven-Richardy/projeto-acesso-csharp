6304de5f2b77cff6fdd419170b40a17e9c63d57f
Stiven Richardy <stivenrichardy445@gmail.com>
2025-11-13 20:03:44 -0300

    Initialize project structure and class skeletons

---
+++ projeto-acesso/Ambiente.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+using projeto_acesso;
+
+/*
+| OBS:
+| a) Cada ambiente registrará, no máximo, 100 logs
+*/
+
+namespace projeto_acesso
+{
+    internal class Ambiente
+    {
+        private int id;
+        private string nome;
+        private Queue<Log> logs;
+
+        public int Id { get => id; set => id = value; }
+        public string Nome { get => nome; set => nome = value; }
+        public Queue<Log> Logs { get => logs; set => logs = value; }
+
+        public Ambiente() { }
+
+        public Ambiente(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+            Logs = new Queue<Log>();
+        }
+
+        public void RegistrarLog(Log log)
+        {
+
+        }
+    }
+}
+
